8d2a81321e59780cc33be0a8ddf48c497d9bc0da
Matt Turner <matt@fiverings.uk>
2022-12-27 15:29:16 +0000

    Reorganize files

--- examples/command_listener.ino
+++ examples/command_listener.ino
@@ -1,4 +1,5 @@
-#include "main.h"
+#include <Eventually.h>
+#include "CommandListener.h"
 
 void setup()
 {
--- src/main.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <Arduino.h>
-#include <Eventually.h>
-#include "CommandReader.h"
